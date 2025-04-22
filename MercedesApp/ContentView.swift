70c28887d608e71e5626636885fdf4273d07aaf4
Yuldashev <ikrom921@gmail.com>
2025-04-22 14:12:47 +0500

    Initial Commit

---
+++ MercedesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MercedesApp
+//
+//  Created by Yuldashev Fazliddin on 22/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MercedesApp/MercedesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercedesAppApp.swift
+//  MercedesApp
+//
+//  Created by Yuldashev Fazliddin on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MercedesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
